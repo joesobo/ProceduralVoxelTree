788f4d88186b3ed20739847ab2c1aa0f735d09ef
Joe Soboleski <soboleskijoe@gmail.com>
2020-07-11 13:58:25 -0700

    SC editor & replaced old canvas SC generate button

---
+++ Assets/Editor/SpaceColonizationEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SpaceColonization))]
+public class SpaceColonizationEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        SpaceColonization spaceColonizer = (SpaceColonization)target;
+
+        if (GUILayout.Button("Generate Tree")) {
+            spaceColonizer.Generate();
+        }
+
+        if (GUILayout.Button("Voxelize Tree")) {
+
+        }
+
+        if (GUILayout.Button("Show Leaves")) {
+
+        }
+
+        if (GUILayout.Button("Save")) {
+
+        }
+    }
+}
